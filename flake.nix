c4380b9cf7b14e3e2e71e3eb3909d3552729c34f
frectonz <fraol0912@gmail.com>
2024-07-29 19:26:38 +0300

    chore: setup devenv

--- flake.nix
+++ flake.nix
@@ -9,9 +9,20 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
+
+    devenv-nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "devenv-nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, beam-utils, ... }@inputs:
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, beam-utils, devenv, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -21,10 +32,27 @@
             (import ./nix/overlay.nix)
           ];
         };
+
+        devenv-shell = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              services.postgres = {
+                enable = true;
+                createDatabase = false;
+                listen_addresses = "127.0.0.1";
+                port = 5432;
+              };
+            }
+          ];
+        };
+
+        devenv-up = self.devShells.${system}.devenv.config.procfileScript;
       in
       {
         devShells = {
           default = pkgs.myCallPackage ./nix/shell.nix { };
+          devenv = devenv-shell;
         };
 
         packages =
@@ -41,7 +69,9 @@
               })
               flake-utils.lib.defaultSystems);
           in
-          { inherit release; } // docker-images;
+          { inherit release devenv-up; } // docker-images;
+
+        formatter = pkgs.nixpkgs-fmt;
       }
     );
 }
