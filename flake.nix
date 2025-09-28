4ccec6a66bd2fd6c939c97b85e1a9ef10a710a1f
frectonz <fraol0912@gmail.com>
2025-09-29 00:55:06 +0300

    chore: fix var name `docker` to `image`

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
 
             version = "0.1.9";
             deploy = pkgs.writeShellScriptBin "deploy" ''
-              ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker-archive:${docker} docker://docker.io/frectonz/devshops_et:${version} --dest-creds="frectonz:$ACCESS_TOKEN"
+              ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker-archive:${image} docker://docker.io/frectonz/devshops_et:${version} --dest-creds="frectonz:$ACCESS_TOKEN"
               ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker://docker.io/frectonz/devshops_et:${version} docker://docker.io/frectonz/devshops_et:latest --dest-creds="frectonz:$ACCESS_TOKEN"
             '';
           in
