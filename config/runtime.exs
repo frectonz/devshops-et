0b30a9bd592aed8206ee84efae694d733ffafefc
frectonz <fraol0912@gmail.com>
2025-09-29 03:27:03 +0300

    feat: use sqlite file

--- config/runtime.exs
+++ config/runtime.exs
@@ -26,9 +26,8 @@ config :devshops_et, admin_chat_id: System.get_env("ADMIN_GROUP_CHAT_ID") |> Str
 
 if config_env() == :prod do
   config :devshops_et, DevshopsEt.Repo,
-    # ssl: true,
-    database: ":memory:",
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+    database: "devshops_et.db",
+    pool_size: 5
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
